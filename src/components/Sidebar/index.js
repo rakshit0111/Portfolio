62cb07e29e2e2c1e0d2b743c0ecad5974ba80605
rakshit0111 <rakshitsingh333@gmail.com>
2024-12-05 23:37:31 +0530

    sidebar done

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,46 @@
+import './index.scss';
+import {Link, NavLink} from 'react-router-dom';
+import LogoS from '../../assets/images/logo-s.png';
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faEnvelope, faHome, faUser} from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+const Sidebar = () =>(
+    <div className = 'nav-bar'>
+        <Link className = 'logo' to='/'>
+            <img src={LogoS} alt='logo'/>
+            <img src={LogoSubtitle} alt='slobodan' className='sub-logo'/>
+        </Link>
+        <nav>
+            <NavLink exact="true" activeclassname = "active" to="/">
+                <FontAwesomeIcon icon={faHome} color='#4d4d4e'/>
+            </NavLink>
+            <NavLink exact="true" activeclassname = "active" className = "about-link" to="/about">
+                <FontAwesomeIcon icon={faUser} color='#4d4d4e'/>
+            </NavLink>
+            <NavLink exact="true" activeclassname = "active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/>
+            </NavLink>
+        </nav>
+        <ul>
+            <li>
+                <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/rakshit-singh-372841282'>
+                    <FontAwesomeIcon  icon={faLinkedin} color='#4d4d4e'/>
+                </a>
+            </li>
+            <li>
+                <a target='_blank' rel='noreferrer' href='https://github.com/rakshit0111'>
+                    <FontAwesomeIcon  icon={faGithub} color='#4d4d4e'/>
+                </a>
+            </li>
+            <li>
+                <a target='_blank' rel='noreferrer' href='https://x.com/Rakshit17220'>
+                    <FontAwesomeIcon  icon={faTwitter} color='#4d4d4e'/>
+                </a>
+            </li>
+        </ul>
+    </div>
+)
+
+export default Sidebar;
